e72d445533a1e1d4065b5c18e1a465b4a845dc7b
olli991 <55631413+olli991@users.noreply.github.com>
2021-04-28 21:59:07 +0200

    Added medium check to also look for USB sticks with toolbox on it. Only possible after initial installation via SD card.

--- toolbox/scripts/util_checksd.sh
+++ toolbox/scripts/util_checksd.sh
@@ -10,6 +10,15 @@ if [ -d /media/mp000/toolbox ]; then
 elif [ -d /media/mp001/toolbox ]; then
     echo "Toolbox SD found"
     export VOLUME=/media/mp001
+elif [ -d /media/mp002/toolbox ]; then
+    echo "Toolbox USB found"
+    export VOLUME=/media/mp002
+elif [ -d /media/mp003/toolbox ]; then
+    echo "Toolbox USB found"
+    export VOLUME=/media/mp003
+elif [ -d /media/mp004/toolbox ]; then
+    echo "Toolbox USB found"
+    export VOLUME=/media/mp004
 else 
     echo "No toolbox SD card found"
     exit 0
