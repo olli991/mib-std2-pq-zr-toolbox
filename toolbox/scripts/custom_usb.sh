6aa6e42c5a974358953b701521d62d92bf552dd5
olli991 <55631413+olli991@users.noreply.github.com>
2021-04-28 22:10:04 +0200

    Added a script to run a custom script from USB and also an entry into the advanced menu to trigger this script.

---
+++ toolbox/scripts/custom_usb.sh
@@ -0,0 +1,27 @@
+#!/bin/ksh
+# This script will run a custom script from USB
+# By Olli
+
+# Info
+DESCRIPTION="This script will run a custom script called toolbox.sh from USB"
+
+echo $DESCRIPTION
+echo 
+sleep 2
+
+# . /eso/hmi/engdefs/scripts/mqb/util_info.sh # For later
+
+# Check if SD card is inserted
+. /tsd/etc/persistence/esd/scripts/util_checksd.sh
+
+# Running script from USB
+echo "Looking for custom toolbox.sh script on USB"
+sleep 1
+
+if [ -f $VOLUME/toolbox.sh ]; then
+	echo "Script found, executing..."
+	. $VOLUME/toolbox.sh
+else
+	echo "Custom script not found. Aborting"
+	exit 0
+fi
