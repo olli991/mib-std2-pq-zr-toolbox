ba6c71febe89ea497d4e7997c6763adf3b31a18c
lprot <lprot@list.ru>
2021-05-20 19:38:00 +0200

    Update

--- toolbox/gem/cpu/onlineservices/1/default/tsd/etc/persistence/esd/scripts/save_sysinfo.sh
+++ toolbox/gem/cpu/onlineservices/1/default/tsd/etc/persistence/esd/scripts/save_sysinfo.sh
@@ -1,5 +1,5 @@
 #!/bin/ksh
-echo "This script saves system info to /dump/sysinfo.txt"
+echo "This script saves system info to sysinfo.txt"
 echo
 
 # Locate Toolbox
@@ -8,19 +8,32 @@ echo
 # Include info utility
 . /tsd/etc/persistence/esd/scripts/util_info.sh
 
-mkdir -p $VOLUME/dump/$VERSION/$SERIAL
+
+if [ ! -d "$VOLUME/dump/$VERSION/$SERIAL" ]; then
+	echo "Creating backup folder..."
+	mkdir -p $VOLUME/dump/$VERSION/$SERIAL
+fi
 
 echo
 echo "Collecting info, please wait..."
 . /tsd/etc/persistence/esd/scripts/sysinfo.sh >$VOLUME/dump/$VERSION/$SERIAL/sysinfo.txt
+
 echo >>$VOLUME/dump/$VERSION/$SERIAL/sysinfo.txt
 echo "*********************************************" >>$VOLUME/dump/$VERSION/$SERIAL/sysinfo.txt
-echo "****************** SLOGINFO *****************" >>$VOLUME/dump/$VERSION/$SERIAL/sysinfo.txt
+echo "**************** SLOGINFO J5 ****************" >>$VOLUME/dump/$VERSION/$SERIAL/sysinfo.txt
+echo "*********************************************" >>$VOLUME/dump/$VERSION/$SERIAL/sysinfo.txt
+on -f J5 sloginfo -t >>$VOLUME/dump/$VERSION/$SERIAL/sysinfo.txt
+
+
+echo >>$VOLUME/dump/$VERSION/$SERIAL/sysinfo.txt
+echo "*********************************************" >>$VOLUME/dump/$VERSION/$SERIAL/sysinfo.txt
+echo "*************** SLOGINFO iMX6 ***************" >>$VOLUME/dump/$VERSION/$SERIAL/sysinfo.txt
 echo "*********************************************" >>$VOLUME/dump/$VERSION/$SERIAL/sysinfo.txt
 sloginfo -t >>$VOLUME/dump/$VERSION/$SERIAL/sysinfo.txt
 
 sync
 
 echo
-echo "Done."
+echo "Done. The file can be found at:"
+echo "/dump/$VERSION/$SERIAL/sysinfo.txt"
 exit 0
