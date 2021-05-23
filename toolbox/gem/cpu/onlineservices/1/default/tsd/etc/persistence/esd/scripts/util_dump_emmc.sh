1bae19d6492f1fe55fa1f5c4bdfe21a3173fde70
lprot <lprot@list.ru>
2021-05-23 23:52:05 +0200

    Fixed eMMC dumper

--- toolbox/gem/cpu/onlineservices/1/default/tsd/etc/persistence/esd/scripts/util_dump_emmc.sh
+++ toolbox/gem/cpu/onlineservices/1/default/tsd/etc/persistence/esd/scripts/util_dump_emmc.sh
@@ -1,6 +1,6 @@
 #!/bin/ksh
 echo "This script dumps eMMC chip content. Make sure SD card or"
-echo "USB drive is formatted in exFat/NTFS as FAT32 has 4Gb limit."
+echo "USB drive is formatted in exFAT/NTFS as FAT32 has 4 GB limit."
 echo
 
 # Locate Toolbox
@@ -13,15 +13,35 @@ cp $VOLUME/toolbox/utils/dd /tmp/
 chmod 777 /tmp/dd
 
 if [ ! -d "$VOLUME/dump/$VERSION/$SERIAL" ]; then
-	echo "Creating backup folder..."
+	echo "Creating dump folder..."
 	mkdir -p $VOLUME/dump/$VERSION/$SERIAL
 fi
 
-echo "Dumping eMMC content to:"
-echo "$VOLUME/dump/$VERSION/$SERIAL/emmc.img"
+if [[ -n "$(mount|grep $VOLUME|grep fat32)" ]]; then
+	echo "ERROR: Please use exFAT or NTFS formatted SD card/USB drive!"
+	exit 0
+fi
+
+OUT=$VOLUME/dump/$VERSION/$SERIAL/emmc.img
+EMMC_SIZE=$(df /dev/hd0 | awk '{printf "%d",$2*512 }' 2>/dev/null)
+echo "eMMC size is $(df /dev/hd0|awk '{printf "%.2f",$2*512/1024/1024/1024}' 2>/dev/null) GB. Dumping to:"
+echo "/dump/$VERSION/$SERIAL/emmc.img"
 echo "It will take some time. Please wait..."
-/tmp/dd if=/dev/hd0 of=$VOLUME/dump/$VERSION/$SERIAL/emmc.img
+start=`date -t`
+/tmp/dd if=/dev/hd0 of=${OUT} bs=524288 &
+LAST_SIZE=""
+while [ -n "$(ps -A|grep dd)" ]; do
+	IMG_SIZE=$(ls -l ${OUT} | awk '{print $5}' 2>/dev/null)
+	if [ -n "$LAST_SIZE" ]; then
+		awk -v E="$EMMC_SIZE" -v B="$IMG_SIZE" -v L="$LAST_SIZE" 'BEGIN{printf "%d%% done, %d MB left, speed %.2f MB/s\n",B/E*100,(E-B)/1024/1024,(B-L)/30/1024/1024}' 2>/dev/null
+	fi
+	LAST_SIZE=$IMG_SIZE
+	sleep 30
+done
+
 rm -f /tmp/dd
 sync
+end=`date -t`
 echo
-echo "Done."
+echo "Done. Time spent: $(((end-start)/60)) minutes."
+exit 0
