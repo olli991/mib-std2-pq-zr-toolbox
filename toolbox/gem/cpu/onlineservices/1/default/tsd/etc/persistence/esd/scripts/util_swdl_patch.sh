dc470967d7a57de5ac56b4eef0b17920e5b1012c
lprot <lprot@list.ru>
2021-06-08 11:06:30 +0200

    Added SWDL patcher

---
+++ toolbox/gem/cpu/onlineservices/1/default/tsd/etc/persistence/esd/scripts/util_swdl_patch.sh
@@ -0,0 +1,55 @@
+#!/bin/ksh
+export TOPIC=swdl
+export MIBPATH=/tsd/bin/swdownload/tsd.mibstd2.system.swdownload
+export SDPATH=patch/$TOPIC/tsd.mibstd2.system.swdownload
+export TYPE="file"
+
+echo "This script will patch tsd.mibstd2.system.swdownload to"
+echo "accept custom metainfo2.txt"
+echo
+
+# Make backup folder
+. /tsd/etc/persistence/esd/scripts/util_backup.sh 
+
+file="/tsd/bin/swdownload/tsd.mibstd2.system.swdownload"
+size=$(ls -l $file | awk '{print $5}' 2>/dev/null)
+
+echo "Checking $file..."
+case $size in
+	3107004) #cpu US PQ 367
+		set -A offsets A9A64 ABA37 ABAF3 14EBDC ;;
+	3063780) #cpuplus US PQ 367
+		set -A offsets A9214 AB1E7 AB2A3 14D6D8 ;;
+	3108412) #cpu EU PQ/ZR 478/479
+		set -A offsets A9A64 ABA37 ABAF3 14EBD4 ;;
+	3065228) #cpuplus EU PQ/ZR 478/479
+		set -A offsets A9234 AB207 AB2C3 14D6F0 ;;
+	*)
+		offsets="" ;;
+esac
+
+if [ -n $offsets ]; then
+	mount -t qnx6 -o remount,rw /dev/hd0t177 /
+	fin=$file
+	set -A bytes 07 EA EA 07
+	j=0
+	for i in ${offsets[@]}; do
+		fh=$((16#$i))
+		echo "Patching offset 0x$i ${bytes[j]}"
+		fout="/tmp/tsd.mibstd2.system.swdownload$j"
+		(head -c $fh $fin; awk -v byte="\x${bytes[j]}" 'BEGIN{printf(byte)}' 2>/dev/null;tail -c +$(($fh+2)) $fin) > $fout
+		if [ $j -gt 0 ]; then
+			rm $fin
+		fi
+		fin=$fout
+		j=$((j+1))
+	done
+	mv -f $fout $file
+	chmod 777 $file
+	sync
+	mount -t qnx6 -o remount,ro /dev/hd0t177 /
+	echo "Patch is applied. Please restart the unit."
+else
+	echo "Unknown $file version detected."
+fi
+exit 0
