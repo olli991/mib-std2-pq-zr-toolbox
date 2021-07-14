d7613dd657ab58cf021b6473f8e594b12934ae65
lprot <lprot@list.ru>
2021-07-14 17:50:20 +0200

    Sysinfo - optimisation for 512Kb RAM units. Network - added internet connection check

---
+++ toolbox/gem/cpu/onlineservices/1/default/tsd/etc/persistence/esd/scripts/test_internet.sh
@@ -0,0 +1,15 @@
+#!/bin/ksh
+echo "This script tests the connection of the unit to the Internet"
+echo
+
+echo "Pinging 8.8.8.8..." &
+wait $!
+ping -c 3 8.8.8.8
+
+echo "Pinging www.google.com..." &
+wait $!
+ping -c 3 8.8.8.8
+
+echo
+echo "Script execution finished."
+exit 0
--- toolbox/gem/cpu/onlineservices/1/default/tsd/etc/persistence/esd/scripts/util_sysinfo.sh
+++ toolbox/gem/cpu/onlineservices/1/default/tsd/etc/persistence/esd/scripts/util_sysinfo.sh
@@ -12,28 +12,33 @@ elif [ "$GEM_SIZE" = "250770" ]; then
 elif [ "$GEM_SIZE" = "250996" ]; then
 	GEM_VERSION="4.12t"
 fi
-echo "Date: $(date) GEM version: $GEM_VERSION"
+echo "Date: $(date) GEM version: $GEM_VERSION Toolbox: 0.8"
 
 #INFO=$(awk -F "" '{for(i=1;i<=NF;i++) if($i~/[A-Z0-9]/) {printf $i} else {printf " "}}' /tsd/var/itr.timer.log 2>/dev/null | sed 's/\b\w\{1,3\}\b\s*//g')
-INFO=$(sloginfo | grep ".devinfo" | tail -1 | sed 's/.*DevInfo: //')
-VCRN=$(sloginfo | grep "VCRN:" | tail -1 | awk '{print $10}' 2>/dev/null)
+sloginfo >/tmp/sloginfo
+INFO=$(grep ".devinfo" /tmp/sloginfo | tail -1 | sed 's/.*DevInfo: //')
+VCRN=$(grep "VCRN:" /tmp/sloginfo | tail -1 | awk '{print $10}' 2>/dev/null)
 if [ -z "$VCRN" ]; then
 	VCRN='----------'
 fi
 
-echo "SW-Version:" $(echo $INFO | awk -F " |=" '{print $2}' 2>/dev/null) "HW-Version:" $(echo $INFO | awk -F " |=" '{print $4}' 2>/dev/null) "PartNum:" $(echo $INFO | awk -F " |=" '{print $6}' 2>/dev/null)
-echo "SerNum:" $(echo $INFO |awk -F " |=" '{print $8}' 2>/dev/null) "Mileage:" $(echo $INFO |awk -F " |=" '{print $12}' 2>/dev/null) "VIN:" $(echo $INFO |awk -F " |=" '{print $14}' 2>/dev/null)
+echo "SW-Version: $(echo $INFO | sed 's/.*SW-Version=\(.*\) HW-Ver.*/\1/' 2>/dev/null) HW-Version: $(echo $INFO | sed 's/.*HW-Version=\(.*\) PartNum=.*/\1/' 2>/dev/null) PartNum: $(echo $INFO | sed 's/.*PartNum=\(.*\) SerNum=.*/\1/' 2>/dev/null)"
+echo "SerNum:" $(echo $INFO | sed 's/.*SerNum=\(.*\) Wdog=.*/\1/' 2>/dev/null) "Mileage:" $(echo $INFO | sed 's/.*Mileage=\(.*\) VIN=.*/\1/' 2>/dev/null) "VIN:" $(echo $INFO | sed 's/.*VIN=\(.*\).*$/\1/' 2>/dev/null)
 echo "Train:" $(awk '/46924065 401 25/ {print $4}' /tsd/var/persistence/.persistence.vault 2>/dev/null | sed 's/[^[:print:]]//g') "VCRN: $VCRN" 
-addr=$((0x020D8000 + 0x024))
-set -A RES $(in32 ${addr})
-gpio5=$((16#${RES[2]}))
-hwVersion=$((${gpio5}&(0x1f)))
-hwVariant=$((((${gpio5}&(1<<5))|((${gpio5}&(0x3f<<10))>>4))>>5))
+
+if [ -e /bin/in32 ]; then
+	addr=$((0x020D8000 + 0x024))
+	set -A RES $(in32 ${addr})
+	gpio5=$((16#${RES[2]}))
+	hwVersion=$((${gpio5}&(0x1f)))
+	hwVariant=$((((${gpio5}&(1<<5))|((${gpio5}&(0x3f<<10))>>4))>>5))
+fi
+
 swdlVariant=$(awk -F "\015| " '/Variant/{print $3}' /tsd/var/swdownload/.swdownload.conf 2>/dev/null)
 swdlHwVersion=$(awk -F "\015| " '/HwVersion/{print $3}' /tsd/var/swdownload/.swdownload.conf 2>/dev/null)
 echo "hwVersion: $hwVersion hwVariant: $hwVariant SWDL hwVersion: $swdlHwVersion SWDL Variant: $swdlVariant" 
 
-RF_MODULE=$(sloginfo | grep "hw version = " | tail -1)
+RF_MODULE=$(grep "hw version = " /tmp/sloginfo | tail -1)
 RF_MODULE_HW=$(echo $RF_MODULE | awk -F 'hw version = |fw version = ' '{printf "%x",strtonum($2)}' 2>/dev/null)
 RF_MODULE_FW=$(echo $RF_MODULE | awk -F 'hw version = |fw version = ' '{printf "%X",strtonum($3)}' 2>/dev/null)
 case $RF_MODULE_HW in
@@ -49,11 +54,12 @@ case $RF_MODULE_HW in
 		RF_MODULE="Unknown, ID: $RF_MODULE_HW, FW: $RF_MODULE_FW" ;;
 esac
 echo "RF module: $RF_MODULE"
-GPS_MODULE=$(sloginfo | awk -F "Info ] " '/\[Info \] GNSS-Module/{print $2}' 2>/dev/null)
+GPS_MODULE=$(awk -F "Info ] " '/\[Info \] GNSS-Module/{print $2}' /tmp/sloginfo 2>/dev/null)
 if [ -n "$GPS_MODULE" ]; then
 	echo $GPS_MODULE
 fi
 
+rm -f /tmp/sloginfo
 echo
 echo "J5 $(on -f J5 /net/imx6/bin/pidin info) "
 echo "$(on -n J5 uname -a)"
@@ -69,7 +75,7 @@ echo "Filesystem                  Size      Used     Avail     Use%  Mounted on"
 df -h
 echo
 echo "J5 root:"
-on -f J5 ls -C /
+ls -C /net/J5/
 echo
 echo "iMX6 root:"
 ls -C /
@@ -79,4 +85,4 @@ ifconfig
 echo
 echo "USB info:"
 usb
-echo "Collection of the information is finished."
+echo "Collection of the information has finished."
