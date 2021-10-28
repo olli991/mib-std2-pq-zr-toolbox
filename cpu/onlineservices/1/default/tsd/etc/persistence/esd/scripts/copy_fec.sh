459ea775a3da9fe459ecc68aa611405edb52a3f7
Mattraks <16359027+Mattraks@users.noreply.github.com>
2021-10-28 15:12:43 +0800

    Fix copy FEC error #35

--- cpu/onlineservices/1/default/tsd/etc/persistence/esd/scripts/copy_fec.sh
+++ cpu/onlineservices/1/default/tsd/etc/persistence/esd/scripts/copy_fec.sh
@@ -1,7 +1,7 @@
 #!/bin/ksh
 export TOPIC=fec
 export MIBPATH=/tsd/etc/slist/signed_exception_list.txt
-export SDPATH=/$TOPIC/signed_exception_list.txt
+export SDPATH=$TOPIC/signed_exception_list.txt
 export TYPE="file"
 
 echo "This script will copy signed_exception_list.txt"
