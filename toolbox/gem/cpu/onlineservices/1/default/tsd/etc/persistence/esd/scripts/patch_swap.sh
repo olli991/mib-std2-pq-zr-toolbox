a1b095c274807fb50e45c633f57f7b1307db2c08
lprot <>
2022-06-29 23:56:23 +0200

    Corrected typo

--- toolbox/gem/cpu/onlineservices/1/default/tsd/etc/persistence/esd/scripts/patch_swap.sh
+++ toolbox/gem/cpu/onlineservices/1/default/tsd/etc/persistence/esd/scripts/patch_swap.sh
@@ -224,7 +224,7 @@ if [ -n "$offsets" ]; then
 		
 		# Mount system partition in read/write mode
 		. /tsd/etc/persistence/esd/scripts/util_mount.sh
-		echo "Creating Exeption List..."
+		echo "Creating Exception List..."
 		
 		mv $fout $MIBPATH
 		chmod 777 $MIBPATH
