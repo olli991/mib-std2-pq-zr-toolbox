62fef8b11d90dc2b92261abeaa51e70b5e7d30c0
lprot <>
2023-05-20 11:21:35 +0200

    Fixed permanent access over wifi

--- cpu/onlineservices/1/default/tsd/etc/persistence/esd/scripts/deactivate_perm_access.sh
+++ cpu/onlineservices/1/default/tsd/etc/persistence/esd/scripts/deactivate_perm_access.sh
@@ -1,18 +1,16 @@
 #!/bin/ksh
-echo "This script activates permanent telnet and console access"
+echo "This script deactivates permanent telnet and console access"
 echo
 sed '/echo \/net\/J5\/dev\/ser1 \\"\/bin\/login -f root\\" qansi-m on >\/tmp\/ttys/d' /tsd/bin/system/startup >/tmp/startup.tmp
 sed '/\/sbin\/tinit -f \/tmp\/ttys \&/d' /tmp/startup.tmp >/tmp/startup1.tmp
 sed '/inetd/d' /tmp/startup1.tmp >/tmp/startup.tmp
 sed '/pass in all/d' /tsd/etc/networking/pf.conf >/tmp/startup1.tmp
+sed '/pass in all/d' /tsd/etc/networking/pf_wlan.conf >/tmp/startup2.tmp
 . /tsd/etc/persistence/esd/scripts/util_mount.sh
-mv -f /tmp/startup.tmp /tsd/bin/system/startup
-chmod 777 /tsd/bin/system/startup
-mv -f /tmp/startup1.tmp /tsd/etc/networking/pf.conf
-chmod 777 /tsd/etc/networking/pf.conf
-sync
+mv -f /tmp/startup.tmp /tsd/bin/system/startup && chmod 777 /tsd/bin/system/startup
+mv -f /tmp/startup1.tmp /tsd/etc/networking/pf.conf && chmod 777 /tsd/etc/networking/pf.conf
+mv -f /tmp/startup2.tmp /tsd/etc/networking/pf_wlan.conf && chmod 777 /tsd/etc/networking/pf_wlan.conf
 . /tsd/etc/persistence/esd/scripts/util_mount_ro.sh
-
 echo
 echo "Permanent telnet and console access are deactivated."
 echo "Please reboot the unit."
