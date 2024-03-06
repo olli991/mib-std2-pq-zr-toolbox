f901b757774c05c8d3e2fff8be1a302e8d6618ef
lprot <>
2024-03-06 20:29:11 +0100

    Added INFO>APP remapping for EU PQ 478 & 480

---
+++ toolbox/gem/cpu/onlineservices/1/default/tsd/etc/persistence/esd/scripts/rb_info2app.sh
@@ -0,0 +1,63 @@
+#!/bin/ksh
+export TOPIC=buttons
+export MIBPATH=/tsd/hmi/HMI/res/configurationmanager.res
+export SDPATH=$TOPIC/configurationmanager.res
+export TYPE="file"
+
+echo "Remapping INFO to APP button..."
+echo
+
+# Include info utility
+. /tsd/etc/persistence/esd/scripts/util_info.sh
+echo "ID: $TRAIN $SYS"
+
+# Make backup folder
+. /tsd/etc/persistence/esd/scripts/util_backup.sh
+
+# Size of the file to patch
+size=$(ls -l $MIBPATH | awk '{print $5}' 2>/dev/null)
+
+echo "Checking $MIBPATH..."
+set -A bytes 72 6B
+offsets=""
+
+case $size in
+	78200) #MST2_EU_VW_PQ_P0478T cpu
+		set -A offsets AAB0 AB30 ;;
+	78202) #MST2_EU_VW_PQ_P0480T cpu
+		set -A offsets AAB2 AB32 ;;
+esac
+
+if [ -n "$offsets" ]; then
+	fin=$MIBPATH
+	j=0
+	for i in ${offsets[@]}; do
+		fh=$((16#$i))
+		echo "Patching offset 0x$i ${bytes[j]}"
+		fout="/tmp/configurationmanager$j"
+		(head -c $fh $fin; awk -v byte="\x${bytes[j]}" 'BEGIN{printf(byte)}' 2>/dev/null;tail -c +$(($fh+2)) $fin) > $fout
+		if [ $j -gt 0 ]; then
+			rm $fin
+		fi
+		fin=$fout
+		j=$((j+1))
+	done
+	
+	if [ -f "$fout" ]; then
+		# Mount system partition in read/write mode
+		. /tsd/etc/persistence/esd/scripts/util_mount.sh
+		echo "Setting file permissions..."
+		
+		mv -f $fout $MIBPATH
+		chmod 777 $MIBPATH
+		
+		# Mount system partition in read/only mode
+		. /tsd/etc/persistence/esd/scripts/util_mount_ro.sh
+		echo "Patch is applied. Please restart the unit."
+	else
+		echo "Patching failed!"
+	fi
+else
+	echo "Unknown file size $size detected."
+fi
+exit 0
