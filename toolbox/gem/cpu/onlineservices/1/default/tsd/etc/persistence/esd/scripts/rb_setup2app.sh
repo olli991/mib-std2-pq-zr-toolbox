4fc5ba4da987628e805451df3c4fb3c65ddb5958
lprot <>
2024-03-11 19:36:41 +0100

    Added SETUP>APP for 369 EU ZR cpuplus

--- toolbox/gem/cpu/onlineservices/1/default/tsd/etc/persistence/esd/scripts/rb_setup2app.sh
+++ toolbox/gem/cpu/onlineservices/1/default/tsd/etc/persistence/esd/scripts/rb_setup2app.sh
@@ -22,6 +22,8 @@ set -A bytes 72 07
 offsets=""
 
 case $size in
+	59520) #MST2_EU_XX_ZR_P0369T cpuplus
+		set -A offsets 7C4C 82B0 ;;
 	67854) #MST2_EU_XX_ZR_P0480T cpuplus
 		set -A offsets 8640 8CA4 ;;
 esac
